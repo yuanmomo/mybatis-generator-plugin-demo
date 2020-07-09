04a6b904dcbb7d827d6a6f801e94a2ebb03c2584
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2020-07-09 10:08:33 +0800

    change mysql driver version && chenge defaultModelType to flat

--- src/test/resources/mbg-init.sql
+++ src/test/resources/mbg-init.sql
@@ -31,6 +31,28 @@ CREATE TABLE `mbg_table` (
   `string_value` varchar(16) DEFAULT '',
   `text_value` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `blob_table`;
+CREATE TABLE `blob_table` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `hash` varchar(256) DEFAULT NULL,
+  `detail` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='blob测试表';
+
+
+DROP TABLE IF EXISTS `multi_key_table`;
+CREATE TABLE `multi_key_table` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `int_value` int NOT NULL,
+  `long_value` bigint unsigned DEFAULT '0',
+  `string_value` varchar(16) DEFAULT '',
+  `text_value` text,
+  PRIMARY KEY (`id`,`int_value`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
 
 SET FOREIGN_KEY_CHECKS = 1;
+
